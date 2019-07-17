93dc712c39bdb6a7d51601ecb003adae329034df
MikeWang <hi.from.mikewang@gmail.com>
2019-07-17 20:02:28 +1200

    init

--- Magic 8 Ball/AppDelegate.swift
+++ Magic 8 Ball/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Magic 8 Ball
 //
-//  Created by 王鑫 on 17/07/19.
-//  Copyright © 2019 王鑫. All rights reserved.
+//  Created by Mike Wang on 17/07/19.
+//  Copyright © 2019 Mike Wang. All rights reserved.
 //
 
 import UIKit
--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,19 +2,36 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by 王鑫 on 17/07/19.
-//  Copyright © 2019 王鑫. All rights reserved.
+//  Created by Mike Wang on 17/07/19.
+//  Copyright © 2019 Mike Wang. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    var ballArray: Array = ["ball1", "ball2","ball3","ball4","ball5"]
 
+    @IBOutlet weak var ballImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateNewImage()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        print("ask clicked")
+        updateNewImage()
+    }
+    
+    private func updateNewImage() {
+        ballImageView.image = UIImage(named: ballArray[createRandomNumber()])
+    }
+    
+    private func createRandomNumber () -> Int {
+        return Int.random(in: 0 ... 4)
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateNewImage()
+    }
 }
 
